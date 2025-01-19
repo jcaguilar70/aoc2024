69023ef6a14a01326e0501c7d09c5c7c6b3e9afb
Justin Aguilar <justin_aguilar@yahoo.com>
2025-01-18 23:39:49 -0700

    added some real code

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
+def x2(x):
+    return x*2
+def fact(n):
+    if n==0:
+        return 1
+    else:
+        return n*fact(n-1)
+a=3
+print(x2(a))
+print("factorial of",a,"is",fact(a))
+
 print("hello world")
